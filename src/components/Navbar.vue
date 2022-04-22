013b354a32203a26cebab7fa8f5a6be809d863a1
peter.chen1024 <u0052017@gmail.com>
2022-04-22 17:10:44 +0800

    fix conflicts 1

---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { RouterLink } from "vue-router";
+import AvatarVue from "./Avatar.vue";
+
+defineProps({
+  size: {
+    type: String,
+    default: "40",
+  },
+  imgUrl: {
+    type: String,
+  },
+  name: {
+    type: String,
+    default: "Unknown",
+  },
+  userPageUrl: {
+    type: String,
+    default: "#",
+  },
+  subTitle: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div class="border-b-2 border-black">
+    <div class="container mx-auto">
+      <div class="h-16 flex justify-between items-center">
+        <h1 class="text-3xl text-black font-paytone">MetaWall</h1>
+        <div class="flex items-center">
+          <AvatarVue size="30" imgUrl="avatars/user.png" />
+          <h2
+            class="text-black font-bold font-azeret border-b-2 border-black ml-2.5"
+          >
+            Member
+          </h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,64 @@
+<template>
+  <NavbarVue class="mb-12" />
+  <div class="container mx-auto">
+    <div class="flex gap-6">
+      <div class="basis-2/3">
+        <div class="flex gap-3">
+          <div class="basis-1/3">
+            <select
+              class="w-full h-11 border-2 border-black"
+              name="category"
+              id="categorySelect"
+            >
+              <option value="newest" selected>最新貼文</option>
+              <option value="popular">熱門貼文</option>
+            </select>
+          </div>
+          <div class="basis-2/3">
+            <div class="flex h-11 border-2 border-black">
+              <input class="w-full" type="text" placeholder="搜尋貼文" />
+              <button
+                type="button"
+                class="bg-primary w-11 flex justify-center items-center border-l-2 border-black"
+              >
+                <IconSearchVue class="w-8 h-8 text-white" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="basis-1/3">
+        <UserInfoVue
+          imgUrl="avatars/user3.png"
+          name="DEMO"
+          userPageUrl="#"
+          :subTitle="num"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from "vue";
+import UserInfoVue from "../components/UserInfo.vue";
+import IconSearchVue from "../components/icons/IconSearch.vue";
+import AvatarVue from "../components/Avatar.vue";
+import NavbarVue from "../components/Navbar.vue";
+
+export default {
+  components: {
+    UserInfoVue,
+    IconSearchVue,
+    AvatarVue,
+    NavbarVue,
+  },
+  setup() {
+    const num = ref(12313131);
+
+    return {
+      num,
+    };
+  },
+};
+</script>
