ce1130bc9b9a7c63b88117cb00c9042fdabee309
Tommy <Tommychen@applyeasypro.com>
2022-04-23 15:37:18 +0800

    [feat] add axios

--- src/main.js
+++ src/main.js
@@ -3,16 +3,19 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "./index.css";
-import { setFakeData } from "../src/mocks/handlers"
+import { setFakeData } from "../src/mocks/handlers";
 import { worker } from "../src/mocks/browser";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 if (import.meta.env.DEV) {
-    setFakeData()
-    worker.start();
+  setFakeData();
+  worker.start();
 }
 
 const app = createApp(App);
 
+app.use(VueAxios, axios);
 app.use(createPinia());
 app.use(router);
 
