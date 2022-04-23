87eaea20ac3e58c9c6292a5411bdceb18e17a8a1
Tommy <Tommychen@applyeasypro.com>
2022-04-23 18:17:43 +0800

    [feat] create posts api methods

--- src/main.js
+++ src/main.js
@@ -7,8 +7,6 @@ import router from "./router";
 import "./index.css";
 import { setFakeData } from "../src/mocks/handlers";
 import { worker } from "../src/mocks/browser";
-import axios from "axios";
-import VueAxios from "vue-axios";
 
 if (import.meta.env.DEV) {
   setFakeData();
@@ -19,7 +17,6 @@ const app = createApp(App);
 
 app.use(VueAxios, axios);
 app.use(createPinia());
-app.use(VueAxios, axios);
 app.use(router);
 
 app.provide("axios", app.config.globalProperties.axios); // provide 'axios'
