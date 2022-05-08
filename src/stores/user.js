0245795b25d979916d78629872a27e6dd244be66
joe <joe.chang1014@gmail.com>
2022-05-08 11:53:57 +0800

    feat: 新增user store

---
+++ src/stores/user.js
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+
+export default defineStore("user", {
+  id: "user",
+  state: () => ({
+    user: {
+      _id: "5566",
+      userName: "joe",
+    },
+  }),
+  actions: {
+    updateUser(obj) {
+      this.user = obj;
+    },
+  },
+});
