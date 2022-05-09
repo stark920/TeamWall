496044b16b320dacf8300c7bb878a6f18596afab
wendy.li <wendy.li@wanin.tw>
2022-05-09 22:17:02 +0800

    新增 留言、點讚 區塊

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,5 +1,7 @@
 <script setup>
 import UserInfo from './UserInfo.vue';
+import IconThumbsUpVue from '@/components/icons/IconThumbsUp.vue';
+import AvatarVue from './Avatar.vue';
 
 defineProps({
   post: {
@@ -27,5 +29,37 @@ defineProps({
       :src="post.updateImage"
       alt="貼文圖片"
     />
+    <div>
+      <button type="button" class="flex items-center justify-center py-5">
+        <IconThumbsUpVue class="mr-2 h-5 w-5 text-primary" />
+        <span> {{ post.likes }}</span>
+      </button>
+    </div>
+    <!--留言-->
+    <div class="flex items-center mb-5">
+      <AvatarVue
+        class="mx-2"
+        size="40"
+        :imgUrl="'https://i.pravatar.cc/150?img=19'"
+      />
+      <div class="flex w-full">
+        <input class="border-2 border-black w-full" type="text" />
+        <button class="border-2 border-black text-base text-white bg-primary inline-block w-full max-w-[128px]">留言</button>
+      </div>
+    </div>
+    <div
+      class="bg-secondary rounded-lg px-4 py-5 mb-4"
+      v-for="(comment, index) in post.comments"
+      :key="index"
+    >
+      <UserInfo
+        class="mb-4"
+        :imgUrl="comment.avatar"
+        :name="comment.userName"
+        userPageUrl="#"
+        :subTitle="comment.createAt"
+      />
+      <p class="mb-4 whitespace-pre">{{ comment.content }}</p>
+    </div>
   </div>
 </template>
