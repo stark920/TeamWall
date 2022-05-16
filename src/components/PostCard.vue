62faabab343e4033ec73d2a75258e201266bdb9d
stark920 <hautang920@gmail.com>
2022-05-16 18:42:20 +0800

    add prettier settings

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -15,11 +15,20 @@ defineProps({
 
 <template>
   <div class="rounded-lg border-2 border-black bg-white p-6">
-    <UserInfo class="mb-4" :imgUrl="post.userId.photo" :name="post.userId.name" userPageUrl="#"
-      :subTitle="post.createAt" />
+    <UserInfo
+      class="mb-4"
+      :imgUrl="post.userId.photo"
+      :name="post.userId.name"
+      userPageUrl="#"
+      :subTitle="post.createAt"
+    />
     <p class="mb-4 whitespace-pre">{{ post.content }}</p>
-    <img class="max-h-96 w-full rounded-lg border-2 border-black object-cover object-center" v-if="post.updateImage"
-      :src="post.updateImage" alt="貼文圖片" />
+    <img
+      class="max-h-96 w-full rounded-lg border-2 border-black object-cover object-center"
+      v-if="post.updateImage"
+      :src="post.updateImage"
+      alt="貼文圖片"
+    />
     <div>
       <button type="button" class="flex items-center justify-center py-5">
         <IconThumbsUpVue class="mr-2 h-5 w-5 text-primary" />
@@ -27,17 +36,33 @@ defineProps({
       </button>
     </div>
     <!--留言-->
-    <div class="flex items-center mb-5">
-      <AvatarVue class="mx-2" size="40" :imgUrl="'https://i.pravatar.cc/150?img=19'" />
+    <div class="mb-5 flex items-center">
+      <AvatarVue
+        class="mx-2"
+        size="40"
+        :imgUrl="'https://i.pravatar.cc/150?img=19'"
+      />
       <div class="flex w-full">
-        <input class="border-2 border-black w-full" type="text" />
+        <input class="w-full border-2 border-black" type="text" />
         <button
-          class="border-2 border-black text-base text-white bg-primary inline-block w-full max-w-[128px]">留言</button>
+          class="inline-block w-full max-w-[128px] border-2 border-black bg-primary text-base text-white"
+        >
+          留言
+        </button>
       </div>
     </div>
-    <div class="bg-secondary rounded-lg px-4 py-5 mb-4" v-for="(comment, index) in post.comments" :key="index">
-      <UserInfo class="mb-4" :imgUrl="comment.avatar" :name="comment.userName" userPageUrl="#"
-        :subTitle="comment.createAt" />
+    <div
+      class="mb-4 rounded-lg bg-secondary px-4 py-5"
+      v-for="(comment, index) in post.comments"
+      :key="index"
+    >
+      <UserInfo
+        class="mb-4"
+        :imgUrl="comment.avatar"
+        :name="comment.userName"
+        userPageUrl="#"
+        :subTitle="comment.createAt"
+      />
       <p class="mb-4 whitespace-pre">{{ comment.content }}</p>
     </div>
   </div>
--- src/views/Foreground/Settings.vue
+++ src/views/Foreground/Settings.vue
@@ -2,63 +2,106 @@
 import { ref } from 'vue';
 import CardTitle from '@/components/CardTitle.vue';
 
-const tabName = ref("editNickName");
+const tabName = ref('editNickName');
 const changeTab = (name) => {
-  tabName.value = name
-}
+  tabName.value = name;
+};
 </script>
-
 
 <template>
   <CardTitle title="修改個人資料"></CardTitle>
-  <ul class="flex flex-wrap text-sm font-medium text-center pl-6">
+  <ul class="flex flex-wrap pl-6 text-center text-sm font-medium">
     <li>
-      <a href="#"
-        class="inline-block py-2 px-6 border-t-2 border-x-2 border-black rounded-t-md bg-white hover:text-white hover:bg-black"
-        :class="tabName === 'editNickName' ? 'text-white bg-black' : ''"
-        @click.prevent="changeTab('editNickName')">
+      <a
+        href="#"
+        class="inline-block rounded-t-md border-x-2 border-t-2 border-black bg-white py-2 px-6 hover:bg-black hover:text-white"
+        :class="tabName === 'editNickName' ? 'bg-black text-white' : ''"
+        @click.prevent="changeTab('editNickName')"
+      >
         暱稱修改
       </a>
     </li>
     <li>
-      <a href="#"
-        class="inline-block py-2 px-6 border-t-2 border-r-2 border-black rounded-t-md bg-white hover:text-white hover:bg-black"
-        :class="tabName === 'resetPassword' ? 'text-white bg-black' : ''"
-        @click.prevent="changeTab('resetPassword')">
+      <a
+        href="#"
+        class="inline-block rounded-t-md border-t-2 border-r-2 border-black bg-white py-2 px-6 hover:bg-black hover:text-white"
+        :class="tabName === 'resetPassword' ? 'bg-black text-white' : ''"
+        @click.prevent="changeTab('resetPassword')"
+      >
         重設密碼
       </a>
     </li>
   </ul>
-  <div class="p-8 border-2 border-black bg-white flex flex-col items-center shadow-post rounded-xl">
+  <div
+    class="flex flex-col items-center rounded-xl border-2 border-black bg-white p-8 shadow-post"
+  >
     <template v-if="tabName === 'editNickName'">
-      <img src="https://fakeimg.pl/107x107" alt="fakeimg" class="rounded-full border-2 border-black mb-4">
-      <input type="button" value="上傳大頭照" class="border-black bg-black px-6 py-1 text-white rounded mb-4">
+      <img
+        src="https://fakeimg.pl/107x107"
+        alt="fakeimg"
+        class="mb-4 rounded-full border-2 border-black"
+      />
+      <input
+        type="button"
+        value="上傳大頭照"
+        class="mb-4 rounded border-black bg-black px-6 py-1 text-white"
+      />
       <form action="" class="">
         <div class="mb-4">
-          <label for="nickName" class="block mb-1">暱稱</label>
-          <input type="text" name="" id="nickName" placeholder="邊緣小杰" class="border-2 border-black">
+          <label for="nickName" class="mb-1 block">暱稱</label>
+          <input
+            type="text"
+            name=""
+            id="nickName"
+            placeholder="邊緣小杰"
+            class="border-2 border-black"
+          />
         </div>
         <div class="mb-8">
-          <label for="male" class="block mb-1">性別</label>
-          <input type="radio" name="sex" id="male" value="male" class="mr-3">
+          <label for="male" class="mb-1 block">性別</label>
+          <input type="radio" name="sex" id="male" value="male" class="mr-3" />
           <label for="male" class="mr-7">男性</label>
-          <input type="radio" name="sex" id="female" value="female" class="mr-3">
+          <input
+            type="radio"
+            name="sex"
+            id="female"
+            value="female"
+            class="mr-3"
+          />
           <label for="female" class="">女性</label>
         </div>
-        <input type="button" value="送出更新" class="border-2 border-black bg-warning text-center py-4 text-black rounded w-full">
+        <input
+          type="button"
+          value="送出更新"
+          class="w-full rounded border-2 border-black bg-warning py-4 text-center text-black"
+        />
       </form>
     </template>
     <template v-else>
       <form action="" class="flex-col items-center">
         <div class="mb-4">
-          <label for="newPassword" class="block mb-1">輸入新密碼</label>
-          <input type="text" id="newPassword" placeholder="請輸入新密碼" class="border-2 border-black">
+          <label for="newPassword" class="mb-1 block">輸入新密碼</label>
+          <input
+            type="text"
+            id="newPassword"
+            placeholder="請輸入新密碼"
+            class="border-2 border-black"
+          />
         </div>
         <div class="mb-8">
-          <label for="checkPassword" class="block mb-1">再次輸入</label>
-          <input type="text" id="checkPassword" placeholder="再次輸入新密碼" class="border-2 border-black">
+          <label for="checkPassword" class="mb-1 block">再次輸入</label>
+          <input
+            type="text"
+            id="checkPassword"
+            placeholder="再次輸入新密碼"
+            class="border-2 border-black"
+          />
         </div>
-        <input type="button" value="重設密碼" class="border-2 border-black bg-subtitle text-white py-4 text-black rounded w-full">
+        <input
+          type="button"
+          value="重設密碼"
+          class="w-full rounded border-2 border-black bg-subtitle py-4 text-white text-black"
+        />
       </form>
     </template>
   </div>
