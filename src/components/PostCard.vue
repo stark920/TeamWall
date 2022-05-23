1969af4eb8c23568008f0a87010325aa2e1e8860
peter.chen1024 <u0052017@gmail.com>
2022-05-23 21:58:01 +0800

    add 按讚貼文

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,12 +1,15 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, inject } from 'vue';
 import UserInfo from './UserInfo.vue';
 import IconThumbsUpVue from '@/components/icons/IconThumbsUp.vue';
+import IconThumbsUpFillVue from '@/components/icons/IconThumbsUpFill.vue';
 import AvatarVue from './Avatar.vue';
+import { API_URL } from '@/global/constant';
 
 const id = ref('6289cb654896923f8331bc15'); // 登入 userId
+const axios = inject('axios');
 
-defineProps({
+const props = defineProps({
   post: {
     type: Object,
     default() {
@@ -14,15 +17,33 @@ defineProps({
     },
   },
 });
+const emit = defineEmits(['get-posts']);
+
+const user = ref({});
+onMounted(() => {
+  user.value = props.post.userId[0] ? props.post.userId[0] : {}; // post.userId 回傳為陣列, 取第一筆資料
+});
+
+const likePost = (postId) => {
+  const data = { posts: postId };
+  axios
+    .post(`${API_URL}/likes/likePost`, data)
+    .then(() => {
+      emit('get-posts');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 </script>
 
 <template>
   <div class="rounded-lg border-2 border-black bg-white p-6 shadow-post">
     <UserInfo
       class="mb-4"
-      :imgUrl="post.userId?.avatar"
-      :name="post.userId?.name"
-      :userPageUrl="`/user/${post.userId?._id}`"
+      :imgUrl="user?.avatar?.url"
+      :name="user?.name"
+      :userPageUrl="`/user/${user?._id}`"
       :subTitle="post.createAt"
     />
     <p class="mb-4 whitespace-pre">{{ post.content }}</p>
@@ -33,12 +54,17 @@ defineProps({
       alt="貼文圖片"
     />
     <div>
-      <button type="button" class="flex items-center justify-center py-5">
+      <button
+        type="button"
+        class="flex items-center justify-center py-5"
+        @click="likePost(post._id)"
+      >
         <!-- 已按讚 icon, 改實心 -->
         <IconThumbsUpVue
-          class="mr-2 h-5 w-5"
-          :class="post.likes?.includes(id) ? 'text-primary' : 'text-black'"
+          v-if="!post.likes?.includes(id)"
+          class="mr-2 h-5 w-5 text-primary"
         />
+        <IconThumbsUpFillVue v-else class="mr-2 h-5 w-5 text-primary" />
         <span> {{ post.likes?.length }}</span>
       </button>
     </div>
