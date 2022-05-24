1b1f7cbd8cbd7198c6e8eff5b1f99592d7698f23
Tommy <tommychen@applyeasypro.com>
2022-05-24 20:31:49 +0800

    feat:新增帳號密碼判斷

--- src/views/signIn.vue
+++ src/views/signIn.vue
@@ -17,7 +17,14 @@
 								type="mail"
 								class="form-input border-2 border-black p-4 w-full"
 								placeholder="Email"
+								@blur="v$.email.$touch"
 							/>
+							<div
+								v-if="v$.email.$errors.length > 0"
+								class="font-secondary text-[#F57375]"
+							>
+								{{ v$.email.$errors[0].$message }}
+							</div>
 						</div>
 						<div class="form-control">
 							<label for="userPassword"></label>
@@ -26,7 +33,14 @@
 								type="password"
 								class="form-input border-2 border-black p-4 w-full"
 								placeholder="Password"
+								@blur="v$.password.$touch"
 							/>
+							<div
+								v-if="v$.password.$errors.length > 0"
+								class="font-secondary text-[#F57375]"
+							>
+								{{ v$.password.$errors[0].$message }}
+							</div>
 						</div>
 					</div>
 					<div class="flex flex-col items-center justify-center">
@@ -47,29 +61,43 @@
 </template>
 
 <script setup>
-import { ref, reactive, watch, onMounted, inject } from 'vue';
+import useVuelidate from '@vuelidate/core';
+import { ref, reactive, computed, onMounted, inject } from 'vue';
 import { RouterLink } from 'vue-router';
 import { useRouter } from 'vue-router';
 import { API_URL } from '@/global/constant';
+import { required, email, helpers } from '@vuelidate/validators';
 const axios = inject('axios');
-
 const router = useRouter();
 const userInfo = reactive({
   email: '',
   password: '',
 });
+const rules = computed(() => ({
+  email: {
+    required: helpers.withMessage('email 必填', required),
+    email: helpers.withMessage('email 格式錯誤', email),
+  },
+  password: {
+    required: helpers.withMessage('密碼必填', required),
+  },
+}));
+const v$ = useVuelidate(rules, userInfo);
 
 const signIn = async () => {
-  const data = userInfo;
-  try {
-    const res = await axios.post(`${API_URL}/user/sign-in`, data);
-    if (res.data.data) {
-      const token = res.headers.authorization.split(' ')[1];
-      localStorage.setItem('signInToken', token);
-      router.push('/');
+  const valid = await v$.value.$validate();
+  if (valid) {
+    const data = userInfo;
+    try {
+      const res = await axios.post(`${API_URL}/user/sign-in`, data);
+      if (res.data.data) {
+        const token = res.headers.authorization.split(' ')[1];
+        localStorage.setItem('signInToken', token);
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    console.log(error);
   }
 };
 
