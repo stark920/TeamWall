6f31e33bcc628e1d950e56a23434165550e434d7
peter.chen1024 <u0052017@gmail.com>
2022-05-29 19:53:47 +0800

    fix: 修正首頁貼文排序

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -4,11 +4,14 @@ import IconThumbsUpVue from '@/components/icons/IconThumbsUp.vue';
 import IconThumbsUpFillVue from '@/components/icons/IconThumbsUpFill.vue';
 import AvatarVue from './Avatar.vue';
 import PostImagesCardVue from './PostImagesCard.vue';
+import { ref, toRaw, watch } from 'vue';
 import { apiLike } from '../utils/apiLike';
+import { apiPost } from '@/utils/apiPost';
 import { useUserStore } from '@/stores';
 const userStore = useUserStore();
+const isLoading = ref(false);
 
-defineProps({
+const props = defineProps({
   post: {
     type: Object,
     default() {
@@ -17,45 +20,75 @@ defineProps({
   },
 });
 
-const emit = defineEmits(['get-posts']);
+const innerPost = ref(toRaw(props.post));
+watch(props, (curr) => {
+  innerPost.value = toRaw(curr.post);
+});
 
+// 按讚貼文
 const likePost = (postId) => {
   const data = { posts: postId };
+  isLoading.value = true;
   apiLike
     .toggle(data)
     .then(() => {
-      emit('get-posts');
+      getPost(postId); // 更新貼文
     })
     .catch((err) => {
+      isLoading.value = false;
+      console.log(err);
+    });
+};
+
+// 取得單筆貼文(更新貼文)
+const getPost = (postId) => {
+  isLoading.value = true;
+  apiPost
+    .getOne(postId)
+    .then((res) => {
+      const [post] = res.data.data; // 回傳是陣列
+      console.log('更新的post:', post);
+      innerPost.value = post;
+      isLoading.value = false;
+    })
+    .catch((err) => {
+      isLoading.value = false;
       console.log(err);
     });
 };
 </script>
 
 <template>
-  <div class="rounded-lg border-2 border-black bg-white p-6 shadow-post">
+  <div
+    class="rounded-lg border-2 border-black bg-white p-6 shadow-post"
+    :class="{ 'animate-pulse': isLoading }"
+  >
     <UserInfo
       class="mb-4"
-      :imgUrl="post.userId?.avatar?.url"
-      :name="post.userId?.name"
-      :userPageUrl="`/profile/${post.userId?._id}`"
-      :subTitle="$filters.dateTime(post.createdAt)"
+      :imgUrl="innerPost.userId?.avatar?.url"
+      :name="innerPost.userId?.name"
+      :userPageUrl="`/profile/${innerPost.userId?._id}`"
+      :subTitle="$filters.dateTime(innerPost.createdAt)"
     />
-    <p class="mb-4 whitespace-pre">{{ post.content }}</p>
-    <PostImagesCardVue v-if="post.image?.length > 0" :images="post.image" />
+    <p class="mb-4 whitespace-pre">{{ innerPost.content }}</p>
+    <PostImagesCardVue
+      v-if="innerPost.image?.length > 0"
+      :images="innerPost.image"
+    />
     <div>
       <button
         type="button"
         class="flex items-center justify-center py-5"
-        @click="likePost(post._id)"
+        @click="likePost(innerPost._id)"
+        :disabled="isLoading"
+        :class="{ 'cursor-not-allowed': isLoading }"
       >
-        <!-- 已按讚 icon, 改實心 -->
         <IconThumbsUpVue
-          v-if="!post.likes?.includes(userStore.user.id)"
+          v-if="!innerPost.likes?.includes(userStore.user.id)"
           class="mr-2 h-5 w-5 text-primary"
         />
         <IconThumbsUpFillVue v-else class="mr-2 h-5 w-5 text-primary" />
-        <span> {{ post.likes?.length }}</span>
+        <span> {{ innerPost.likes?.length }}</span>
       </button>
     </div>
     <!--留言-->
@@ -76,7 +109,7 @@ const likePost = (postId) => {
     </div>
     <div
       class="mb-4 rounded-lg bg-secondary px-4 py-5"
-      v-for="(comment, index) in post.comments"
+      v-for="(comment, index) in innerPost.comments"
       :key="index"
     >
       <UserInfo
