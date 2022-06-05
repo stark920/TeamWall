aa3c22613d354fb80c0773bcd2c4448b5960a8f1
Genos Huang <38322680+stark920@users.noreply.github.com>
2022-06-05 16:31:41 +0800

    deploy

---
+++ assets/CardTitle.c767508e.js
@@ -0,0 +1,1 @@
+import{a as t,c as r,j as o}from"./index.4ae37eee.js";const a={class:"shadow-block relative mb-8 flex items-center justify-center border-2 border-black bg-white p-5 font-azeret text-xl font-bold"},i={props:{title:{type:String}},setup(e){return(s,l)=>(t(),r("div",a,o(e.title),1))}};export{i as _};
