df7d79a14b06b6114d994967a50f75c74fa3652c
Genos Huang <38322680+stark920@users.noreply.github.com>
2022-06-06 16:54:03 +0800

    deploy

--- assets/ChatRoomView.c53cc84c.js
+++
@@ -1,1 +0,0 @@
-import{P as e,Q as t,a,f as n,q as c,a7 as m}from"./index.b345e2aa.js";const l={setup(f){const r=e(),{room:s}=t(r);return(u,p)=>{var o;return a(),n(m,{roomInfo:(o=c(s))==null?void 0:o[0]},null,8,["roomInfo"])}}};export{l as default};
